21c3288deec48a0b8fbf7988737cd283fc476d69
Diogo Soares <dsoares365@gmail.com>
2025-04-09 19:20:13 -0300

    build 2

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
+set -o errexit  # Faz o script falhar se algum comando falhar
 
 pip install -r requirements.txt
 
-# Garante que o ambiente do Django seja usado corretamente
 export DJANGO_SETTINGS_MODULE=pi2025.settings
 
-python manage.py makemigrations apconect
-python manage.py migrate
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput
