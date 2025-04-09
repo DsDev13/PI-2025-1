9a381d3015add85e19c2b506fa8a96237e37dc00
Diogo Soares <dsoares365@gmail.com>
2025-04-09 19:11:33 -0300

    Correcao build.sh migracoes

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
+
+# Garante que o ambiente do Django seja usado corretamente
+export DJANGO_SETTINGS_MODULE=pi2025.settings
+
+python manage.py makemigrations apconect
 python manage.py migrate
 python manage.py collectstatic --noinput
