7b10868465402ba378391c6ba7a066cd20a14830
Diogo Soares <dsoares365@gmail.com>
2025-04-16 23:00:11 -0300

    Forçando migrações

--- pi2025/settings.py
+++ pi2025/settings.py
@@ -104,3 +104,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Redirecionamentos de login/logout
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
+
+import django
+from django.core.management import call_command
+
+try:
+    django.setup()
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print("Erro ao aplicar migrations automaticamente:", e)
