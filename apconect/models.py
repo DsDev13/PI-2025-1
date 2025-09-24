7ecb366b7ac4843e9b7fdb0f3a6c2c4bceb4c605
Marcone-Santos <marconemagalhaes203@gmail.com>
2025-09-24 19:35:09 -0300

    address ok

--- apconect/models.py
+++ apconect/models.py
@@ -18,3 +18,15 @@ class Anuncio(models.Model):
 
     def __str__(self):
         return f"{self.titulo} ({self.usuario.username})"
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    cep = models.CharField(max_length=9)
+    logradouro = models.CharField(max_length=255)
+    numero = models.CharField(max_length=20)
+    bairro = models.CharField(max_length=100)
+    cidade = models.CharField(max_length=100)
+    estado = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.user.username
